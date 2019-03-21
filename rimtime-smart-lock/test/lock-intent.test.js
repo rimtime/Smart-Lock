965960a0daedf3c980f44ce60f305875b051cc8c
rimtime <richard.marshall@gmail.com>
2019-03-21 14:39:11 -0400

    I included lockIntent test scripts but not yet the corresponding intents in the model

---
+++ rimtime-smart-lock/test/lock-intent.test.js
@@ -0,0 +1,37 @@
+'use strict';
+const { App, Util } = require('jovo-framework');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { Alexa } = require('jovo-platform-alexa');
+jest.setTimeout(500);
+
+for (const p of [new Alexa(), new GoogleAssistant()]) {
+    const testSuite = p.makeTestSuite();
+
+    describe(`PLATFORM: ${p.constructor.name} INTENTS` , () => {
+        test('should return the LOCKED status at "lockIntent"', async () => {
+            const conversation = testSuite.conversation();
+
+            const lockRequest = await testSuite.requestBuilder.intent('lockIntent', { lockStatus : 'lock'});
+            const responseLockRequest = await conversation.send(lockRequest)
+            expect(
+                responseLockRequest.isTell(expectedLockPrompt)
+            ).toBe(true);
+
+        });
+    });
+    describe(`PLATFORM: ${p.constructor.name} INTENTS` , () => {
+        test('should return the UNLOCKED status at "lockIntent"', async () => {
+            const conversation = testSuite.conversation();
+
+            const lockRequest = await testSuite.requestBuilder.intent('lockIntent', { lockStatus : 'unlock'});
+            const responseLockRequest = await conversation.send(lockRequest)
+            expect(
+                responseLockRequest.isTell(expectedUnLockPrompt)
+            ).toBe(true);
+
+        });
+    });
+}
+
+let expectedLockPrompt = 'Your door is locked.'
+let expectedUnLockPrompt = 'Your door is unlocked.'
